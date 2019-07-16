93d01acd0ee165fad1a6dec5a0c98b401fc2886d
Vaibhav Singh <vaibhavsinghacads@gmail.com>
2019-07-16 10:54:37 -0400

    Create Solution.java

---
+++ googlepractice/136.SingleNumber/Solution.java
@@ -0,0 +1,10 @@
+// Bitwise XOR of all numbers cancels pairs out.
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int i : nums) {
+            result ^= i;
+        }
+        return result;
+    }
+}
